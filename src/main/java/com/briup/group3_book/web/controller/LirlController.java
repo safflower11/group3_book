4b35050efbcaf24d191255095e59649ac1a814b4
lirl <2849569929@qq.com>
2019-12-24 17:29:52 +0800

    新增

--- src/main/java/com/briup/group3_book/web/controller/LirlController.java
+++ src/main/java/com/briup/group3_book/web/controller/LirlController.java
@@ -1,4 +1,35 @@
 package com.briup.group3_book.web.controller;
 
+import com.briup.group3_book.bean.book;
+import com.briup.group3_book.util.Message;
+import com.briup.group3_book.util.MessageUtil;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LirlController {
+    @PostMapping("/add")
+    @ApiOperation("添加书籍")
+    public Message add(book b){
+        return MessageUtil.success("添加书籍成功");
+    }
+    @GetMapping("/delete")
+    @ApiOperation("删除书籍")
+    public Message delete(book b){
+        return MessageUtil.success("删除书籍成功");
+
+    }
+    @GetMapping("/search")
+    @ApiOperation("查询书籍")
+    public Message search(book b){
+        return MessageUtil.success("查询书籍成功");
+    }
+    @PostMapping("/update")
+    @ApiOperation("更新书籍")
+    public Message update(book b){
+        return MessageUtil.success("更新书籍成功");
+
+    }
 }
