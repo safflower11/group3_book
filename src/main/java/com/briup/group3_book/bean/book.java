1c016fb40c44cd3faa7c48e6e93c64ec78b37035
liuy <908657326@qq.com>
2019-12-24 17:10:48 +0800

    修改了book类

--- src/main/java/com/briup/group3_book/bean/book.java
+++ src/main/java/com/briup/group3_book/bean/book.java
@@ -1,4 +1,56 @@
 package com.briup.group3_book.bean;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class book {
+    @ApiModelProperty(value = "书籍编号")
+    private int book_id;//书籍编号
+    @ApiModelProperty(value = "作者")
+    private String autor;//作者
+    @ApiModelProperty(value = "书籍名字")
+    private String name;
+    @ApiModelProperty(value = "出版社")
+    private String address;//出版社
+    @ApiModelProperty(value = "书籍描述")
+    private String desp;//描述
+
+    public int getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(int book_id) {
+        this.book_id = book_id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDesp() {
+        return desp;
+    }
+
+    public void setDesp(String desp) {
+        this.desp = desp;
+    }
 }
