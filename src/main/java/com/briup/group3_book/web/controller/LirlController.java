aa19398119a7918321ccde75fc16fb5be0a8eb06
lirl <2849569929@qq.com>
2019-12-25 08:57:39 +0800

    新增

--- src/main/java/com/briup/group3_book/web/controller/LirlController.java
+++ src/main/java/com/briup/group3_book/web/controller/LirlController.java
@@ -6,9 +6,11 @@ import com.briup.group3_book.util.MessageUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/lirl")
 public class LirlController {
     @PostMapping("/add")
     @ApiOperation("添加书籍")
